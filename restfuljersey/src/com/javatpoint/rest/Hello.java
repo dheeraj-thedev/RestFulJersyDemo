b5e412bb899d4ff666efc26259987a25d05e4b3c
Dheeraj Singh <bond.deejay@gmail.com>
2020-05-09 18:03:46 +0530

    first commit: added JSON functionality

--- restfuljersey/src/com/javatpoint/rest/Hello.java
+++ restfuljersey/src/com/javatpoint/rest/Hello.java
@@ -6,25 +6,27 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 
 @Path("/hello")
 public class Hello {
 
+	
+	// Akash this is working fine for XML 
+	//we need to add a mapper that will explain this object to json ;
+	
+	
 	@POST
 	@Path("/test")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Student getStudentDetails(@QueryParam("name") String name, @QueryParam("collegeName") String collegeName) {
+	public String getStudentDetails(@QueryParam("name") String name, @QueryParam("collegeName") String collegeName) {
 		System.out.println("Message Received");
 		System.out.println(name + "fjdlkfd" + collegeName);
 		Student student = new Student();
 		student.setName(name);
 		student.setCollegeName(collegeName);
 		System.out.println(student);
-		// return
-		// Response.status(Status.OK).entity(student).build();//student.getCollegeName();
-		return student;
+		// return Response.status(Status.OK).entity(student).build();//student.getCollegeName();
+		return student.getJSON();
 	}
 
 	@GET
--- restfuljersey/src/com/javatpoint/rest/Student.java
+++ restfuljersey/src/com/javatpoint/rest/Student.java
@@ -3,11 +3,11 @@ package com.javatpoint.rest;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-
 @XmlRootElement(name = "student")
 public class Student {
 	private String name;
 	private String collegeName;
+
 	@XmlElement
 	public String getName() {
 		return name;
@@ -16,6 +16,7 @@ public class Student {
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	@XmlElement
 	public String getCollegeName() {
 		return collegeName;
@@ -23,5 +24,9 @@ public class Student {
 
 	public void setCollegeName(String collegeName) {
 		this.collegeName = collegeName;
+	}
+
+	public String getJSON() {
+		return "{" + "name:" + this.getName() + ",\ncollegeName:" + this.getCollegeName() + "" + "}";
 	}
 }
