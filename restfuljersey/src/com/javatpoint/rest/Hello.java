fdfc3b6fa4a7d11308e118fc84b878e1b2b5c735
Dheeraj Singh <bond.deejay@gmail.com>
2020-05-09 17:49:31 +0530

    first commit: adding query param

--- restfuljersey/src/com/javatpoint/rest/Hello.java
+++ restfuljersey/src/com/javatpoint/rest/Hello.java
@@ -1,29 +1,51 @@
 package com.javatpoint.rest;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
 @Path("/hello")
 public class Hello {
-  @GET
-  @Produces(MediaType.TEXT_PLAIN)
-  public String sayPlainTextHello() {
-    return "Hello Jersey Plain";
-  }
-  // This method is called if XML is request
-  @GET
-  @Produces(MediaType.TEXT_XML)
-  public String sayXMLHello() {
-    return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
-  }
 
-  // This method is called if HTML is request
-  @GET
-  @Produces(MediaType.TEXT_HTML)
-  public String sayHtmlHello() {
-    return "<html> " + "<title>" + "Hello Jersey" + "</title>"
-        + "<body><h1>" + "Hello Jersey HTML" + "</h1></body>" + "</html> ";
-  }
+	@POST
+	@Path("/test")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Student getStudentDetails(@QueryParam("name") String name, @QueryParam("collegeName") String collegeName) {
+		System.out.println("Message Received");
+		System.out.println(name + "fjdlkfd" + collegeName);
+		Student student = new Student();
+		student.setName(name);
+		student.setCollegeName(collegeName);
+		System.out.println(student);
+		// return
+		// Response.status(Status.OK).entity(student).build();//student.getCollegeName();
+		return student;
+	}
 
-} 
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String sayPlainTextHello() {
+		return "Hello Jersey Plain";
+	}
+
+	// This method is called if XML is request
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	public String sayXMLHello() {
+		return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
+	}
+
+	// This method is called if HTML is request
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String sayHtmlHello() {
+		return "<html> " + "<title>" + "Hello Jersey" + "</title>" + "<body><h1>" + "Hello Jersey HTML" + "</h1></body>"
+				+ "</html> ";
+	}
+
+}
---
+++ restfuljersey/src/com/javatpoint/rest/Student.java
@@ -0,0 +1,27 @@
+package com.javatpoint.rest;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@XmlRootElement(name = "student")
+public class Student {
+	private String name;
+	private String collegeName;
+	@XmlElement
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	@XmlElement
+	public String getCollegeName() {
+		return collegeName;
+	}
+
+	public void setCollegeName(String collegeName) {
+		this.collegeName = collegeName;
+	}
+}
